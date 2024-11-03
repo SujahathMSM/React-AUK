5489e5f21437490f6226c8ce03238311f1db56b0
sujahathmsm <sujahathmhmd@gmail.com>
2024-11-03 22:37:03 +0530

    Components and Props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1 className="header">Hello Sujahath</h1>
       <p className="text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
         exercitationem ea totam quos dolor temporibus a, odio cumque repellendus
         laborum?
       </p>
+      <Footer name="Sujahath" age="26" city="beruwala" job="SE" langauge="JavaScript"/>
+      <Footer name="Sujahath M S" age="22" city="Signapore" job="SSE" langauge="Pyhton"/>
+      <Footer name="Sujahath SMM" age="24" city="Colombo" job="ASE" langauge="Java"/>
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="buttons-nav">
+      <button className="btn">Click</button>
+      <button className="btn">Next</button>
+      <button className="btn">About</button>
+    </div>
+  );
+};
+
+export default Header;
