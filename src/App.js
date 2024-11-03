62b05414d2e90e10d0153dee51a586e63a1b7b83
sujahathmsm <sujahathmhmd@gmail.com>
2024-11-03 21:09:29 +0530

    Initial Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Hello Sujahath</h1>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        exercitationem ea totam quos dolor temporibus a, odio cumque repellendus
+        laborum?
+      </p>
     </div>
   );
 }
